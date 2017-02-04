a9d9d80b646011d7e8e6ab833f29faf606001500
PingNZ <wenpingy@outlook.com>
2017-02-04 17:45:59 +1300

    V 0.1

--- test.py
+++ test.py
@@ -3,14 +3,17 @@
 
 import requests
 
-url = 'http://www.6park.com'
-html = requests.get(url)
-html.encoding = "gb2312" # need to check the webpage's encode - charset
-print(html.text) 
+# url = 'http://www.6park.com'
+# html = requests.get(url)
+# html.encoding = "gb2312" # need to check the webpage's encode - charset
+# print(html.text) 
 
 
 url2 = 'http://www.skykiwi.com'
 html2 = requests.get(url2)
-html2.encoding = "gbk"
-print(html2.text)
+print (html2.encoding)
+print (html2.status_code)
+# html2.encoding = "gbk"
+# print(html2.text)
+
 
