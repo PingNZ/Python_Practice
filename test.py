445efe1919e625c6b919dfe13550146e43728d82
PingNZ <wenpingy@outlook.com>
2017-02-04 14:50:18 +1300

    Added Skykiwi

--- test.py
+++ test.py
@@ -6,6 +6,11 @@ import requests
 url = 'http://www.6park.com'
 html = requests.get(url)
 html.encoding = "gb2312" # need to check the webpage's encode - charset
-print(html.text)
+print(html.text) 
 
+
+url2 = 'http://www.skykiwi.com'
+html2 = requests.get(url2)
+html2.encoding = "gbk"
+print(html2.text)
 
